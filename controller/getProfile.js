1e4bf80adb59104a8aba52e1744ceaff31bc72ef
Ridwan Yinusa <ridwanyinusa3@gmail.com>
2025-01-30 22:22:43 +0100

    make changes to my varaible declared in the github url

--- controller/getProfile.js
+++ controller/getProfile.js
@@ -1,19 +1,16 @@
 const getProfile = (req, res) => {
-    try {
-
-        const profile = {
-            email: "ridwany@gmail.com",
-            current_datetime: new Date().toISOString(),
-            github_url: "https://github.com/Olanrewaju-0123?tab=repositories"
-        }
-        res.status(200).json(profile)
-    } catch (err) {
-        res.status(500).json({
-            message: err.message
-
-        })
-
-    }
+  try {
+    const profile = {
+      email: "ridwany@gmail.com",
+      current_datetime: new Date().toISOString(),
+      github_url: "https://github.com/Olanrewaju-0123/Hng_Backend",
+    };
+    res.status(200).json(profile);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
 };
 
 module.exports = { getProfile };
